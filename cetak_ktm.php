1cb66d0d6c1bc6cb975ad0fda9a8074c2ec445f0
yusriazraa <yusri.azra@gmail.com>
2023-12-19 21:33:25 +0700

    Done
    pembuatan ktm STMIK & UNPER

---
+++ cetak_ktm.php
@@ -0,0 +1,78 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Cetak KTM</title>
+    <link href="bootstrap-5.3.2-dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+        $bulan_indonesia = array(
+            "Januari",
+            "Februari",
+            "Maret",
+            "April",
+            "Mei",
+            "Juni",
+            "Juli",
+            "Agustus",
+            "September",
+            "Oktober",
+            "November",
+            "Desember"
+        );
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            // Cek apakah ada input yang kosong
+            if (empty($_POST["nim"]) || empty($_POST["nama"]) || empty($_POST["jenis_kelamin"]) || empty($_POST["tempat_lahir"]) || empty($_POST["tanggal_lahir"]) || empty($_POST["prodi"])) {
+                echo "<script>alert('Harap lengkapi semua kolom pada form!'); window.location.href='index.php';</script>";
+                exit;
+            }
+        }
+        // Ambil tanggal dari inputan user
+        $tanggal_lahir = $_POST['tanggal_lahir'];
+
+        // Pecah tanggal menjadi tiga bagian
+        $pecah_tanggal = explode("-", $tanggal_lahir);
+
+        // Ubah format tanggal
+        $tanggal_baru = $pecah_tanggal[2] . " " . $bulan_indonesia[$pecah_tanggal[1] - 1] . " " . $pecah_tanggal[0];
+
+    ?> 
+    <div class="container">
+        <div class="row justify-content-center p-5">
+            <div class="col-md-6 bg-light p-3">
+                <div class="printIn">
+                    <img src="img/ktm-stmik.png" class="ktmImage rounded-5">
+                    <div class="textIn text-white">
+                        <h6 class="nim"> <?= $_POST['nim'] ?> </h6>
+                        <h6 class="nama"><?= $_POST['nama'] ?> </h6>
+                        <h6 class="ttl"> <?php echo"$_POST[tempat_lahir], $tanggal_baru";?> </h6>
+                        <h6 class="prodi"> <?= $_POST['prodi'] ?> </h6>
+                    </div>
+                    <img class="photoIn" src="<?= $_POST['jenis_kelamin'] == "Perempuan" ? 'img/botak.png' : 'img/restu.png' ?>" alt="" srcset="">
+                </div>
+            </div>
+            <div class="col-md-6 bg-light p-3">
+                <div class="printIn">
+                    <img src="img/ktm.jpg" class="ktmImage rounded-5">
+                    <div class="textIn">
+                        <h6 class="nim"> <?= $_POST['nim'] ?> </h6>
+                        <h6 class="nama"><?= $_POST['nama'] ?> </h6>
+                        <h6 class="ttl"> <?php echo"$_POST[tempat_lahir], $tanggal_baru";?> </h6>
+                        <h6 class="prodi"> <?= $_POST['prodi'] ?> </h6>
+                    </div>
+                    <img class="photoIn" src="<?= $_POST['jenis_kelamin'] == "Perempuan" ? 'img/singer-guitar-6.png' : 'img/one-punch-man.png' ?>" alt="" srcset="">
+                </div>
+            </div>
+            <a href="index.php" class="btn btn-danger mt-4 w-100 border border-1 border-white">Kembali</a>
+        </div>
+    </div>
+</body>
+<script>
+    print();
+</script>
+</html>
---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form Cetak KTM</title>
+    <link href="bootstrap-5.3.2-dist/css/bootstrap.min.css" rel="stylesheet" >
+    <link rel="stylesheet" href="css/style.css">
+    <script src="bootstrap-5.3.2-dist/js/bootstrap.bundle.min.js" ></script>
+</head>
+<body >
+    <div class="container ">
+        <div class="row justify-content-center p-5">
+            <div class="col-md-3">
+                <img class="w-100" src="img/logo-stmik.png" alt="">
+            </div>
+            <div class="col-md-5 bg-light p-4">
+                <img src="" alt="">
+                <h3 class="text-center">Form Cetak KTM</h3>
+                <form method="POST" action="cetak_ktm.php">
+                    <div class="mb-3 mt-3">
+                        <label for="nim" class="form-label">NIM :</label>
+                        <input type="text" class="form-control" id="nim" placeholder="Masukan NIM" name="nim">
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label for="nama" class="form-label">Nama Lengkap :</label>
+                        <input type="text" class="form-control" id="nama" placeholder="Masukan Nama Lengkap" name="nama">
+                    </div>
+                    <div class="mb-3 mt-3">
+                        <label for="jenis_kelamin" class="form-label">Jenis Kelamin :</label>
+                        <select for="jenis_kelamin" class="form-control" id="jenis_kelamin" name="jenis_kelamin">
+                            <option value=""> --- Pilih Jenis Kelamin ---</option>
+                            <option value="Laki-laki"> Laki-laki</option>
+                            <option value="Perempuan"> Perempuan</option>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="tempat_lahir" class="form-label">Tempat Lahir :</label>
+                        <input type="text" class="form-control" id="tempat_lahir" placeholder="Masukan Tempat Lahir" name="tempat_lahir">
+                    </div>
+                    <div class="mb-3">
+                        <label for="tanggal_lahir" class="form-label">Tanggal Lahir :</label>
+                        <input type="date" class="form-control" id="tanggal_lahir" placeholder="Masukan Tanggal Lahir" name="tanggal_lahir">
+                    </div>
+                    <div class="mb-3">
+                        <label for="prodi" class="form-label">Program Studi : </label>
+                        <select for="prodi" class="form-control" id="prodi" name="prodi">
+                            <option value="">--- Pilih Program Studi ---</option>
+                            <option value="Teknik Informatika"> Teknik Informatika</option>
+                            <option value="Teknik Sipil"> Teknik Sipil</option>
+                            <option value="Agro Teknologi"> Agro Teknologi</option>
+                            <option value="Agri Bisnis"> Agri Bisnis</option>
+                        </select>
+                    </div>
+                    <button type="submit" class="btn btn-success mt-4">Cetak KTM</button>
+                </form>
+            </div>
+            <div class="col-md-3"><img class="w-100" src="img/logo-unper.png" alt=""></div>
+        </div>
+    </div>
+</body>
+</html>
